0a9c89a8da6ab63ef7c77e9115cf620452c58ac1
Akumar <128833380+Adityaakr@users.noreply.github.com>
2024-09-07 20:54:51 +0530

    fixed issues: Added new content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,21 @@
 'use client'
 
 import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import MorphLogo from './logo'
 
 function App() {
-  const account = useAccount()
-  const { connectors, connect, status, error } = useConnect()
-  const { disconnect } = useDisconnect()
 
   return (
     <>
-      <div>
-        <h2>Account</h2>
-        <div>
-          status: {account.status}
-          <br />
-          addresses: {JSON.stringify(account.addresses)}
-          <br />
-          chainId: {account.chainId}
-        </div>
-
-        {account.status === 'connected' && (
-          <button type="button" onClick={() => disconnect()}>
-            Disconnect
-          </button>
-        )}
-      </div>
-
-      <div>
-        <h2>Connect</h2>
-        {connectors.map((connector) => (
-          <button
-            key={connector.uid}
-            onClick={() => connect({ connector })}
-            type="button"
-          >
-            {connector.name}
-          </button>
-        ))}
-        <div>{status}</div>
-        <div>{error?.message}</div>
-      </div>
+     <div className='m-10'>
+     <MorphLogo/>
+     </div>
+     <div className='flex justify-center items-center 
+     h-screen text-center font-bold text-6xl text-[#14a800] 
+     leading-snug'>
+      Start Building your next Dapp
+     <br className='mt-5'/> On Morph🐨
+     </div>
     </>
   )
 }
