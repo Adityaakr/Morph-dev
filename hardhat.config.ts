b8dbbdeb5d6572f5ee1b5068c4abf4db8695a300
Akumar <128833380+Adityaakr@users.noreply.github.com>
2024-09-08 02:53:43 +0530

    Updated

---
+++ hardhat.config.ts
@@ -0,0 +1,20 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+  paths: {
+    sources: "./contract/contracts",
+    tests: "./contract/test",
+    cache: "./contract/cache",
+    artifacts: "./contract/artifacts"
+  },
+  networks: {
+    morph: {
+      url: "https://rpc-quicknode-holesky.morphl2.io",
+      accounts: ["4018d6281651978c80c56841b67abc24e62ff0ce6cfea87c1f89e6fe8b4e3bb0"]
+    }
+  }
+};
+
+export default config;
