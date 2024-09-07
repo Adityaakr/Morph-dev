919a1a330c80454e3f01b291383f25516627c4a7
Akumar <128833380+Adityaakr@users.noreply.github.com>
2024-09-08 02:32:02 +0530

    fixed hardhat config error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+   /** @type {import('next').NextConfig} */
+   const nextConfig = {
+    reactStrictMode: true,
+    webpack: (config) => {
+      config.externals.push("hardhat");
+      return config;
+    },
+  }
 
-module.exports = nextConfig
+  module.exports = nextConfig
